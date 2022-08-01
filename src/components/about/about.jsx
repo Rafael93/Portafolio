d96151dec7a528ab211393b49c05b1eaf24ab16c
Rafael Gamarra <rafaelgamas@gmail.com>
2022-08-01 12:01:25 -0400

    update1-08

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,23 +2,24 @@ import React from 'react'
 
 const About = () => {
 	return (
-  	<div name="about" className='w-full h-screen bg-[#0a192f] text-gray-300 pr-5'>
+  	<div name="about" className='w-full h-screen bg-[#0a192f] text-gray-300 pr-10'>
 			<div className='flex flex-col justify-center items-center w-full h-full'>
-				<div className='max-w-[100px] w-full grid grid-cols-2 gap-10'>
+				<div className='max-w-[1000px] w-full grid grid-cols-2 gap-10'>
 					<div className='sm:text-right pb-8 pl-4'>
 						<p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
 					</div>
-					<div>
-					</div>
-					<div>
+					<div></div>
+				</div>
+				<div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+					<div className='text-right sm:text-right text-4xl font-bold'>
 						<p>Hi. I'm Rafael nice to meet you. Please take a look around.</p>
 					</div>
-					<div>
+					<div className='sm:text-right text-2xl'>
 						<p>I'm passionate about building apps that improves the lives of those who will use it.</p>
 					</div>
 				</div>
 			</div>
-    </div>
+		</div>
   )
 }
 
