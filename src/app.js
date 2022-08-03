d1f24e4787a3051249cc32dca274f0ee15e936c7
Rafael Gamarra <rafaelgamas@gmail.com>
2022-08-03 11:56:34 -0400

    update3-08

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import './app.css';
 import Nav from './components/nav/nav';
 import Home from './components/home/home';
 import About from './components/about/about';
+import Skills from './components/Skills/skills';
 
 /*FUENTE https://www.youtube.com/watch?v=2kg0z1qNrkw*/
 
@@ -11,6 +12,7 @@ function App(){
       <Nav />
       <Home />
       <About />
+      <Skills />
     </div>
   )
 }
---
+++ src/components/Skills/skills.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import HTML from '../../assets/html.png' 
+import CSS from '../../assets/css.png'
+import Javascript from '../../assets/javascript.png'
+import ReactLogo from '../../assets/react.png'
+import Mongo from '../../assets/mongo.png'
+import Node from '../../assets/node.png'
+import Github from '../../assets/github.png'
+import Tailwind from '../../assets/tailwind.png'
+
+const Skills = () => {
+  return (
+    <div name="skills" className="bg-[#0a192f] text-gray-300">
+			{/* CONTAINER */}
+			<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+				<div>
+					<p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+					<p className='py-4'>// these are the tecnologies I've worked with</p>
+				</div>
+
+				<div className='w-full grid grid-cols-2 sm:grid-cols-4 gap 4 text-center py-8'>
+					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+						<img className='w-20 mx-auto' src={HTML} alt="html icon" />
+						<p>HTML</p>
+					</div>
+					<div className='mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+						<img className='w-20 mx-auto' src={CSS} alt="html icon" />
+						<p className='my-5'>CSS</p>
+					</div>
+					<div className='mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+						<img className='w-20 mx-auto' src={Javascript} alt="html icon" />
+						<p className='my-5'>Javascript</p>
+					</div>
+					<div className='mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+						<img className='w-20 mx-auto' src={ReactLogo} alt="html icon" />
+						<p className='my-5'>React</p>
+					</div>
+					<div className='mt-8 mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+						<img className='w-20 mx-auto' src={Mongo} alt="html icon" />
+						<p className='my-5'>Mongo DB</p>
+					</div>
+					<div className='mt-8 mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+						<img className='w-20 mx-auto' src={Node} alt="html icon" />
+						<p className='my-5'>Node Js.</p>
+					</div>
+					<div className='mt-8 mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+						<img className='w-20 mx-auto' src={Github} alt="html icon" />
+						<p className='my-5'>Github</p>
+					</div>
+					<div className='mt-8 mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+						<img className='w-20 mx-auto' src={Tailwind} alt="html icon" />
+						<p className='my-5'>Tailwind CSS</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+  )
+}
+
+export default Skills
