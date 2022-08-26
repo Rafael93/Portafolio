ba30f054a194e48bfb5d7e49b5cb341aac2083e1
Rafael Gamarra <rafaelgamas@gmail.com>
2022-08-26 12:01:57 -0400

    update26-08-22

--- src/components/Skills/skills.jsx
+++ src/components/Skills/skills.jsx
@@ -10,28 +10,28 @@ import Tailwind from '../../assets/tailwind.png'
 
 const Skills = () => {
   return (
-    <div name="skills" className="bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
 			{/* CONTAINER */}
-			<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+			<div className='max-w-[1000px] p-4 flex flex-col mx-[100px] w-full h-full md:mx-auto content-center'>
 				<div>
 					<p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
 					<p className='py-4'>// these are the tecnologies I've worked with</p>
 				</div>
 
 				<div className='w-full grid grid-cols-2 sm:grid-cols-4 gap 4 text-center py-8'>
-					<div className='shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+					<div className='my-4 mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
 						<img className='w-20 mx-auto' src={HTML} alt="html icon" />
 						<p>HTML</p>
 					</div>
-					<div className='mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+					<div className='my-4 mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
 						<img className='w-20 mx-auto' src={CSS} alt="html icon" />
 						<p className='my-5'>CSS</p>
 					</div>
-					<div className='mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+					<div className='my-4 mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
 						<img className='w-20 mx-auto' src={Javascript} alt="html icon" />
 						<p className='my-5'>Javascript</p>
 					</div>
-					<div className='mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
+					<div className='my-4 mx-4 shadow-md shadow-[#040c16] hover:scale-110 duration-100'>
 						<img className='w-20 mx-auto' src={ReactLogo} alt="html icon" />
 						<p className='my-5'>React</p>
 					</div>
